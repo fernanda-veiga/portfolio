1d4ceda706e0c9ec92a5d3dc59836b97d02c8a6a
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-06-22 17:10:55 -0300

    Added a NavBar component and its elements.

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <p>Fernanda Veiga</p>
+      <a>Home</a>
+      <a>Skills</a>
+      <a>Projects</a>
+      <a>Contact</a>
+    </div>
+  );
+}
+
+export default NavBar;
