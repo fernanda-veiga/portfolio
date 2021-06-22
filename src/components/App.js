37b03926cb2a5984d564990d4aa17b1cc23dcabb
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-06-22 17:11:40 -0300

    Moved the App component to the components folder. Added the AboutMe and NavBar components to the App component.

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "./NavBar";
+import AboutMe from "./AboutMe";
+import "../style/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <AboutMe />
+    </div>
+  );
+}
+
+export default App;
