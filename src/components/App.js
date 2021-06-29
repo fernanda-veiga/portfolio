636ecf91a9161d903f2968a8b56b7f67b5635ea5
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-06-29 17:51:08 -0300

    Added a function that checks wheter the user scrolled down and changes the state isTopOfPage between true or false. Depending on the value, the color of the navbar will change.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import AboutMe from "./AboutMe";
 import Projects from "./Projects";
 import "../style/App.css";
 
 function App() {
+  let listener;
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+
+  useEffect(() => {
+    listener = document.addEventListener("scroll", () => {
+      const pixelsScrolled = document.scrollingElement.scrollTop;
+
+      if (pixelsScrolled >= 50) {
+        setIsTopOfPage(false);
+      } else {
+        setIsTopOfPage(true);
+      }
+    });
+
+    return () => {
+      document.removeEventListener("scroll", listener);
+    };
+  }, [isTopOfPage]);
+
   return (
     <div className="App">
-      <NavBar />
+      <NavBar isTopOfPage={isTopOfPage} />
       <AboutMe />
       <Projects />
     </div>
